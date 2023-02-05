c1bfa7dbc21586112aae70422990ea631d990aef
Angel <angelantoinetted@yahoo.com>
2023-02-04 21:36:48 -0500

    created two urls with diff param values

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 //
+//Highlevel Goal:
+//show page one
+//click once to show new url in window location
+//click again to change page two
+//**for a better demo, possibly add "searching for dlp in params" 
+//before changing to the 2nd page 
 //
-//
+
+//Highlevel: create url
+//show in console
+    const href = document.getElementById('link');
+    const firstHref = document.getElementById('firstLink');
+    console.log(href)
+    console.log(firstHref)
+
 // test of switch page
 //listen for click to hide one div
 addEventListener('click', () => {

567cd259030b730d9afe47568dfb7e6d1e69589e
Angel <angelantoinetted@yahoo.com>
2023-02-04 21:04:37 -0500

    hide div on click

---
+++ main.js
@@ -0,0 +1,11 @@
+//
+//
+//
+// test of switch page
+//listen for click to hide one div
+addEventListener('click', () => {
+    document.querySelector('.one').style.display = 'none';
+    document.querySelector('.two').style.display = 'block';
+    console.log('switch');
+
+})
